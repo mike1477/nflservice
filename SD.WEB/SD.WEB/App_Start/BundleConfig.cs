e5b28717bad546944437b26c77613a3882ac75e2
Michal Harrison <mlh1477@gmail.com>
2019-05-30 13:53:36 -0400

    Style home page

--- SD.WEB/SD.WEB/App_Start/BundleConfig.cs
+++ SD.WEB/SD.WEB/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace SD.WEB
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/Site.css"));
         }
     }
 }
